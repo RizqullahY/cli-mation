ff9ede72396875897ca2bfefc75dc56ce0a63f13
raflyasliGalek <104709795+RizqullahY@users.noreply.github.com>
2025-08-13 10:09:58 +0700

    Update viko-ascii.sh

--- viko-ascii.sh
+++ viko-ascii.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Sembunyikan kursor saat animasi
 tput civis
 
+# Loop tanpa henti
 while true; do
     curl -s https://raw.githubusercontent.com/RizqullahY/cli-mation/master/viko-ascii.txt | \
-    awk -v RS='====' '{system("clear"); print; system("sleep 0.1")}'
+    awk -v RS='====' '{print; system("sleep 0.1")}'
 done
 
+# Tampilkan kursor lagi saat keluar
 trap 'tput cnorm' EXIT
